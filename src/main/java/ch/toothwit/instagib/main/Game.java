7dc3bc33fa3e2796848b6b3fbebe6fa7f4d46959
fredo121 <fredr121>
2015-11-06 23:54:49 +0100

    Added Scoreboard , removed unused BarAPI

--- src/main/java/ch/toothwit/instagib/main/Game.java
+++ src/main/java/ch/toothwit/instagib/main/Game.java
@@ -18,161 +18,201 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective; 
+import org.bukkit.scoreboard.Score; 
+import org.bukkit.scoreboard.Scoreboard;
 
 import ch.toothwit.lobby.main.LobbyAPI;
 import ch.toothwit.lobby.main.LobbyEventHandler;
+
 import net.md_5.bungee.api.ChatColor;
 
-public class Game implements LobbyEventHandler { 
-	private static Game instance; 
-	private HashMap<String, GamePlayer> gamePlayers = new HashMap<String, GamePlayer>(); 
-	private HashMap<String, Integer> cooldowns = new HashMap<String, Integer>(); 
-	private GameState gameState; 
-	 
-	public Game(){ 
-		LobbyAPI.subscribe(this); 
-		this.reload(); 
-		new BukkitRunnable() { 
-			public void run() { 
-				for(Iterator<Map.Entry<String, Integer>> it = cooldowns.entrySet().iterator(); it.hasNext(); ) { 
-					Map.Entry<String, Integer> entry = it.next(); 
-					if(entry.getValue().intValue() <= 0) { 
-						it.remove(); 
-				    } 
-					else{
-						entry.setValue(entry.getValue()-1); 
-					}
-				} 
-			} 
-		}.runTaskTimer(Instagib.get(), 0L, 2L); 
-	} 
+public class Game implements LobbyEventHandler {
+	private static Game instance;
+	private HashMap<String, GamePlayer> gamePlayers = new HashMap<String, GamePlayer>();
+	private HashMap<String, Integer> cooldowns = new HashMap<String, Integer>();
+	private GameState gameState;
+	private Scoreboard scoreboard; 
+	private Objective objective; 
 	
-	public static Game get(){
-		if(instance == null){
-			instance = new Game(); 
-		} 
-		return instance; 
-	} 
-	
-	private void reload(){
-		LobbyAPI.reload(); 
-		Bukkit.getScheduler().cancelTask(countdownTask); 
-		
-		this.gameState = GameState.LOBBY; 
-		this.gamePlayers = new HashMap<String, GamePlayer>(); 
-	} 
-	
-	public void setCooldown(Player player){
-		cooldowns.put(player.getUniqueId().toString(), 16); 
+	public Scoreboard getScoreboard() {
+		return scoreboard;
 	}
-	
-	public boolean getCooldown(Player player){
-		return cooldowns.get(player.getUniqueId().toString()) != null; 
-	}
-	
-	int countdownTask; 
-	
-	@SuppressWarnings("deprecation") 
-	public void StartGame(List<Player> players) { 
-		ItemStack weapon = new ItemStack(Material.STICK); 
-		ItemMeta meta = weapon.getItemMeta(); 
-		meta.setDisplayName("Rifle"); 
-		weapon.setItemMeta(meta); 
-		for(Player player : players){ 
-			Inventory inventory = player.getInventory(); 
-			this.gamePlayers.put(player.getUniqueId().toString(), new GamePlayer(player)); 
-			inventory.clear(); 
-			inventory.addItem(weapon); 
-		} 
-		this.gameState = GameState.RUNNING; 
-		
-		int n=0; 
-		List<Location> locations = Settings.get().getSpawnLocations(); 
-		Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', Settings.get().getString("gameStarted"))); 
-		for(Player player : Bukkit.getOnlinePlayers()){ 
-			player.teleport(locations.get(n%locations.size())); 
-			n++; 
-		} 
-		
-		countdownTask = Bukkit.getScheduler().scheduleSyncRepeatingTask(Instagib.get(), new BukkitRunnable() { 
-			private int timeLeft = Settings.get().getGameDuration(); 
-			public void run() {
-				timeLeft--; 
-				if(timeLeft > 60 && timeLeft % 60 == 0){
-					Bukkit.broadcastMessage(MessageFormat.format(ChatColor.translateAlternateColorCodes('&', Settings.get().getString("minutesLeft")), timeLeft/60)); 
-				}
-				if(timeLeft == 60){
-					Bukkit.broadcastMessage(MessageFormat.format(ChatColor.translateAlternateColorCodes('&', Settings.get().getString("minuteLeft")), 1)); 
-				}
-				if(timeLeft == 30){
-					Bukkit.broadcastMessage(MessageFormat.format(ChatColor.translateAlternateColorCodes('&', Settings.get().getString("secondsLeft")), timeLeft)); 
-				}
-				if(timeLeft <= 5 && timeLeft > 0){
-					Bukkit.broadcastMessage(MessageFormat.format(ChatColor.translateAlternateColorCodes('&', Settings.get().getString("secondsLeft")), timeLeft)); 
-				}
-				if(timeLeft <= 0){ 
-					Game.get().endGame(); 
-				}
-			} 
-		}, 0L, 20L); 
-	} 
-	
-	public GamePlayer getGamePlayer(Player player){ 
-		return gamePlayers.get(player.getUniqueId().toString());  
-	} 
-	
-	public void onPlayerKilled(Player shooter, Player victim){
-		getGamePlayer(victim).deaths++; 
-		getGamePlayer(victim).respawn(); 
-		getGamePlayer(shooter).kills++; 
-		Bukkit.broadcastMessage(MessageFormat.format(ChatColor.translateAlternateColorCodes('&', Settings.get().getString("killed")), victim.getName(), shooter.getName())); 
-	}
-	
-	@SuppressWarnings({ "deprecation" })
-	public void endGame(){ 
-		Bukkit.getScheduler().cancelTask(countdownTask); 
-		gameState = GameState.FINISHED; 
-		
-		List<GamePlayer> ranked = new ArrayList<GamePlayer>(); 
-		
-		for(Entry<String, GamePlayer> entry : this.gamePlayers.entrySet()) {
-			ranked.add(entry.getValue());  
-		}
-		
-		Collections.sort(ranked, new Comparator<GamePlayer>(){
-		     public int compare(GamePlayer o1, GamePlayer o2){
-		         if(o1.kills == o2.kills)
-		             return 0;
-		         return o1.kills > o2.kills ? -1 : 1;
-		     }
-		}); 
-		
-		int n=1 ; 
-		Bukkit.broadcastMessage(ChatColor.GOLD+"===================================="); 
-		for(GamePlayer gamePlayer : ranked){ 
-			Bukkit.broadcastMessage(ChatColor.RED+"        "+n+""+ChatColor.GOLD+". "+gamePlayer.player.getName()+" ["+gamePlayer.kills+"/"+gamePlayer.deaths+"]"); 
-			n++; 
-		} 
-		Bukkit.broadcastMessage(ChatColor.GOLD+"===================================="); 
-		
-		Bukkit.broadcastMessage(MessageFormat.format(ChatColor.translateAlternateColorCodes('&', Settings.get().getString("lobbyMessage")), 5)); 
-		Bukkit.getScheduler().runTaskLater(Instagib.get(), new BukkitRunnable() { 
-			public void run() {                
-				for(Player p : Bukkit.getOnlinePlayers()){ 
-					p.getInventory().clear(); 
-					p.setResourcePack("http://collab.toothwit.ch/emptyPack.zip"); 
-					Util.SendToBungeeServer(LobbyAPI.getBungeeLobbyServer(), p); 
-				} 
-				Game.get().reload(); 
-			} 
-		}, 5*20L); 
-	}
-	
-	public void setGameState(GameState gameState) { 
-		this.gameState = gameState; 
-	} 
 
-	public GameState getGameState() { 
+	public Game() {
+		LobbyAPI.subscribe(this);
+		this.reload();
+		new BukkitRunnable() {
+			public void run() {
+				for (Iterator<Map.Entry<String, Integer>> it = cooldowns.entrySet().iterator(); it.hasNext();) {
+					Map.Entry<String, Integer> entry = it.next();
+					if (entry.getValue().intValue() <= 0) {
+						it.remove();
+					} else {
+						entry.setValue(entry.getValue() - 1);
+					}
+				}
+			}
+		}.runTaskTimer(Instagib.get(), 0L, 2L);
+	}
+
+	public static Game get() {
+		if (instance == null) {
+			instance = new Game();
+		}
+		return instance;
+	}
+
+	private void reload() {
+		LobbyAPI.reload();
+		Bukkit.getScheduler().cancelTask(countdownTask);
+
+		this.gameState = GameState.LOBBY;
+		this.gamePlayers = new HashMap<String, GamePlayer>();
+		this.scoreboard = Bukkit.getScoreboardManager().getMainScoreboard(); 
+		scoreboard.clearSlot(DisplaySlot.SIDEBAR); 
+	}
+
+	public void setCooldown(Player player) {
+		cooldowns.put(player.getUniqueId().toString(), 16);
+	}
+
+	public boolean getCooldown(Player player) {
+		return cooldowns.get(player.getUniqueId().toString()) != null;
+	}
+
+	int countdownTask;
+
+	@SuppressWarnings("deprecation")
+	public void StartGame(List<Player> players) {
+		ItemStack weapon = new ItemStack(Material.STICK);
+		ItemMeta meta = weapon.getItemMeta();
+		meta.setDisplayName("Rifle");
+		weapon.setItemMeta(meta); 
+		
+		this.objective = this.scoreboard.getObjective("test"); 
+		if(this.objective == null){
+			this.objective = this.scoreboard.registerNewObjective("test", "dummy"); 
+		}
+		this.objective.setDisplaySlot(DisplaySlot.SIDEBAR); 
+		this.objective.setDisplayName("Kills"); 
+		
+		for (Player player : players) {
+			Inventory inventory = player.getInventory();
+			this.gamePlayers.put(player.getUniqueId().toString(), new GamePlayer(player)); 
+			
+			Score score = this.objective.getScore(player.getDisplayName()); 
+			score.setScore(0); 
+			
+			inventory.clear();
+			inventory.addItem(weapon);
+		}
+		this.gameState = GameState.RUNNING;
+
+		int n = 0;
+		List<Location> locations = Settings.get().getSpawnLocations();
+		Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', Settings.get().getString("gameStarted")));
+		for (Player player : Bukkit.getOnlinePlayers()) {
+			player.teleport(locations.get(n % locations.size()));
+			n++;
+		}
+
+		countdownTask = Bukkit.getScheduler().scheduleSyncRepeatingTask(Instagib.get(), new BukkitRunnable() {
+			private int timeLeft = Settings.get().getGameDuration();
+
+			public void run() {
+				timeLeft--;
+				if (timeLeft > 60 && timeLeft % 60 == 0) {
+					Bukkit.broadcastMessage(MessageFormat.format(
+							ChatColor.translateAlternateColorCodes('&', Settings.get().getString("minutesLeft")),
+							timeLeft / 60));
+				}
+				if (timeLeft == 60) {
+					Bukkit.broadcastMessage(MessageFormat.format(
+							ChatColor.translateAlternateColorCodes('&', Settings.get().getString("minuteLeft")), 1));
+				}
+				if (timeLeft == 30) {
+					Bukkit.broadcastMessage(MessageFormat.format(
+							ChatColor.translateAlternateColorCodes('&', Settings.get().getString("secondsLeft")),
+							timeLeft));
+				}
+				if (timeLeft <= 5 && timeLeft > 0) {
+					Bukkit.broadcastMessage(MessageFormat.format(
+							ChatColor.translateAlternateColorCodes('&', Settings.get().getString("secondsLeft")),
+							timeLeft));
+				}
+				if (timeLeft <= 0) {
+					Game.get().endGame();
+				}
+			}
+		}, 0L, 20L);
+	}
+
+	public GamePlayer getGamePlayer(Player player) {
+		return gamePlayers.get(player.getUniqueId().toString());
+	}
+
+	public void onPlayerKilled(Player shooter, Player victim) {
+		getGamePlayer(victim).deaths++;
+		getGamePlayer(victim).respawn();
+		getGamePlayer(shooter).kills++;
+		Bukkit.broadcastMessage(
+				MessageFormat.format(ChatColor.translateAlternateColorCodes('&', Settings.get().getString("killed")),
+						victim.getName(), shooter.getName())); 
+		Score score = this.objective.getScore(shooter.getDisplayName()); 
+		score.setScore(getGamePlayer(shooter).kills); 
+	}
+
+	@SuppressWarnings({ "deprecation" })
+	public void endGame() {
+		Bukkit.getScheduler().cancelTask(countdownTask);
+		gameState = GameState.FINISHED;
+
+		List<GamePlayer> ranked = new ArrayList<GamePlayer>();
+
+		for (Entry<String, GamePlayer> entry : this.gamePlayers.entrySet()) {
+			ranked.add(entry.getValue());
+		}
+
+		Collections.sort(ranked, new Comparator<GamePlayer>() {
+			public int compare(GamePlayer o1, GamePlayer o2) {
+				if (o1.kills == o2.kills)
+					return 0;
+				return o1.kills > o2.kills ? -1 : 1;
+			}
+		});
+
+		int n = 1;
+		Bukkit.broadcastMessage(ChatColor.GOLD + "====================================");
+		for (GamePlayer gamePlayer : ranked) {
+			Bukkit.broadcastMessage(ChatColor.RED + "        " + n + "" + ChatColor.GOLD + ". "
+					+ gamePlayer.player.getName() + " [" + gamePlayer.kills + "/" + gamePlayer.deaths + "]");
+			n++;
+		}
+		Bukkit.broadcastMessage(ChatColor.GOLD + "====================================");
+
+		Bukkit.broadcastMessage(MessageFormat
+				.format(ChatColor.translateAlternateColorCodes('&', Settings.get().getString("lobbyMessage")), 5));
+		Bukkit.getScheduler().runTaskLater(Instagib.get(), new BukkitRunnable() {
+			public void run() {
+				for (Player p : Bukkit.getOnlinePlayers()) {
+					p.getInventory().clear();
+					p.setResourcePack("http://collab.toothwit.ch/emptyPack.zip");
+					Util.SendToBungeeServer(LobbyAPI.getBungeeLobbyServer(), p);
+				}
+				Game.get().reload();
+			} 
+		}, 5 * 20L); 
+		
+		scoreboard.clearSlot(DisplaySlot.SIDEBAR); 
+	}
+
+	public void setGameState(GameState gameState) {
+		this.gameState = gameState;
+	}
+
+	public GameState getGameState() {
 		return this.gameState;
-	} 
-} 
+	}
+}
